5cfd4b67ac20474bda7e4cf456affc497a4dc47c
ovais <movaisr07@gmail.com>
2022-02-06 00:37:39 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import io from 'socket.io-client';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { useEffect, useState } from 'react';
+import {
+    Line,
+    CartesianGrid,
+    BarChart,
+    Bar,
+    LineChart,
+    XAxis,
+    YAxis,
+    Tooltip,
+} from 'recharts'
+
+const socket = io('http://localhost:8080',{
+    transports: ['websocket', 'polling']
+})
+
+const  App= ()=>{
+    const [ data, setData] = useState([])
+    //1. listen for a cpu event and update the state
+    useEffect(()=>{
+        
+        socket.on('cpu', (cpuPercent)=>{
+            
+            setData((val)=>[...val, cpuPercent])
+            
+        })
+    }, [])
+    return(
+        <div>
+            <h1>Real Time CPU Usage</h1>
+            <LineChart width={500} height={300} data={data}>
+             <XAxis dataKey="name"/>
+                <YAxis/>
+            {/* <CartesianGrid stroke="#eee" strokeDasharray="5 5"/> */}
+            <Line  dataKey="value" />
+  </LineChart>
+    
+        </div>
+    )
 }
 
-export default App;
+export default App
